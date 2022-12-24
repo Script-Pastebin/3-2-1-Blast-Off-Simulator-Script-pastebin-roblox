db3e2614d48d05b23261189e101661377f5c7ad3
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 00:32:21 +0100

    commit zero

---
+++ 3-2-1 Blast Off Simulator Script pastebin roblox 3-2-1 Blast Off Simulator .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Dk4Q/Dk4QHub/master/Loader", true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							There is one here: Fuel processor amount, auto sell fuel, auto buy rocket, auto buy backpack, auto buy fuelscoop, island teleport.
+						
+]]
